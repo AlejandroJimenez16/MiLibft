e7ecb62a379b3f78ca9bcde79169c06ac14dab49
Alejandro Jimenez Garcia <alejandj@c2r8s2.42madrid.com>
2025-04-01 17:17:23 +0200

    ft_strlen repetido, lo he borrado

--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -6,23 +6,11 @@
 /*   By: alejandj <alejandj@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/29 12:59:09 by alejandj          #+#    #+#             */
-/*   Updated: 2025/03/06 12:19:48 by alejandj         ###   ########.fr       */
+/*   Updated: 2025/04/01 14:10:04 by alejandj         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-
-size_t	ft_strlen(const char *s)
-{
-	int	i;
-
-	if (!s)
-		return (0);
-	i = 0;
-	while (s[i])
-		i++;
-	return (i);
-}
 
 char	*ft_strjoin(char const *s1, char const *s2)
 {
