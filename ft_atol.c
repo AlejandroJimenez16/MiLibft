204e21cefcf0aaa2c5e4cea4ec4cc24d326c0760
Alejandro Jimenez Garcia <alejandj@c2r14s2.42madrid.com>
2025-05-20 16:26:13 +0200

    Atol arreglado

--- ft_atol.c
+++ ft_atol.c
@@ -6,7 +6,7 @@
 /*   By: alejandj <alejandj@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/24 14:23:27 by alejandj          #+#    #+#             */
-/*   Updated: 2025/04/24 14:26:56 by alejandj         ###   ########.fr       */
+/*   Updated: 2025/05/20 16:24:14 by alejandj         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,7 @@ long	ft_atol(const char *nptr)
 	int		i;
 	long	result;
 	int		sign;
+	int		digit;
 
 	i = 0;
 	result = 0;
@@ -29,7 +30,12 @@ long	ft_atol(const char *nptr)
 		i++;
 	while (nptr[i] >= '0' && nptr[i] <= '9')
 	{
-		result = result * 10 + (nptr[i] - '0');
+		digit = nptr[i] - '0';
+		if (sign == 1 && (result > (LONG_MAX - digit) / 10))
+			return (LONG_MAX);
+		if (sign == -1 && (-result < (LONG_MIN + digit) / 10))
+			return (LONG_MIN);
+		result = result * 10 + digit;
 		i++;
 	}
 	return (result * sign);
